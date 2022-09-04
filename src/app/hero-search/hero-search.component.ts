020b4a239a55a83886d42ada0249ed5717467d0a
MaetDol <rndka9@gmail.com>
2022-09-05 07:53:25 +0900

    구현한 리액트 컴포넌트를 렌더링하게 변경

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
+import { Component, ElementRef, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  Observable,
+  Subject,
+  switchMap,
+} from "rxjs";
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
+import { ReactRenderService } from "../react/react-render.service";
+import { HeroSearch } from "./hero-search.react-component";
 
 @Component({
-  selector: 'app-hero-search',
-  templateUrl: './hero-search.component.html',
-  styleUrls: ['./hero-search.component.css']
+  selector: "app-hero-search",
+  templateUrl: "./hero-search.component.html",
+  styleUrls: ["./hero-search.component.css"],
 })
 export class HeroSearchComponent implements OnInit {
-
   heroes$!: Observable<Hero[]>;
   private searchTerms = new Subject<string>();
 
   constructor(
-    private heroService: HeroService
-  ) { }
+    private heroService: HeroService,
+    private router: Router,
+    private reactRenderService: ReactRenderService,
+    private hostElementRef: ElementRef
+  ) {}
+
+  ngAfterViewInit() {
+    this.reactRenderService.render(this.hostElementRef, HeroSearch, {
+      heroService: this.heroService,
+      router: this.router,
+    });
+  }
 
   search(term: string) {
     this.searchTerms.next(term);
@@ -28,5 +46,4 @@ export class HeroSearchComponent implements OnInit {
       switchMap((term: string) => this.heroService.searchHeroes(term))
     );
   }
-
 }
